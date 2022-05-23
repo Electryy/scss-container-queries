5bbdac04eff44a922a93fac2ab8b595dad3f6a34
Markus Kumpulainen <kumpulainen.markus@gmail.com>
2022-05-23 14:56:34 +0300

    made it to a module

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import { WidthEncoder } from "./resizer";
+
+const encoder = WidthEncoder();
+
+encoder.start();
+encoder.stop();
+
+console.log(encoder);
--- src/js/resizer.js
+++ src/js/resizer.js
@@ -1,33 +1,45 @@
-let elements = document.querySelectorAll("[data-cq]");
-const resizeObserver = new ResizeObserver((entries) => {
-  for (let entry of entries) {
-    const width = Math.round(entry.contentRect.width);
+export function WidthEncoder(selectorString) {
+  const selector = selectorString ? selectorString : "[data-cq]";
+  const elements = document.querySelectorAll(selector);
 
-    // Convert to numbers array and reverse
-    // 123 becomes [3,2,1]
-    let widths = width.toString().split("").reverse();
+  const resizeObserver = new ResizeObserver((entries) => {
+    for (let entry of entries) {
+      const width = Math.round(entry.contentRect.width);
 
-    // Set the data attributes. Example: data-cq-one="0-1-2-3-4"
-    entry.target.setAttribute("data-cq-one", convertToCssNumber(widths[0]));
-    entry.target.setAttribute("data-cq-ten", convertToCssNumber(widths[1]));
-    entry.target.setAttribute("data-cq-hun", convertToCssNumber(widths[2]));
-    entry.target.setAttribute("data-cq-tho", convertToCssNumber(widths[3]));
-  }
-});
+      // Convert to numbers array and reverse
+      // 123 becomes [3,2,1]
+      let widths = width.toString().split("").reverse();
 
-function convertToCssNumber(number) {
-  // widths array might not have the index. This means it's 0.
-  if (!number) {
-    return "0";
+      // Set the data attributes. Example: data-cq-one="0-1-2-3-4"
+      entry.target.setAttribute("data-cq-one", convertToCssNumber(widths[0]));
+      entry.target.setAttribute("data-cq-ten", convertToCssNumber(widths[1]));
+      entry.target.setAttribute("data-cq-hun", convertToCssNumber(widths[2]));
+      entry.target.setAttribute("data-cq-tho", convertToCssNumber(widths[3]));
+    }
+  });
+
+  function convertToCssNumber(number) {
+    // widths array might not have the index. This means it's 0.
+    if (!number) {
+      return "0";
+    }
+    let numberString = "";
+    for (let i = 0; i <= number; i++) {
+      numberString += i;
+    }
+    // Convert 01234 -> 0-1-2-3-4
+    return numberString.toString().split("").join("-");
   }
-  let numberString = "";
-  for (let i = 0; i <= number; i++) {
-    numberString += i;
+
+  function start() {
+    elements.forEach((element) => {
+      resizeObserver.observe(element);
+    });
   }
-  // Convert 01234 -> 0-1-2-3-4
-  return numberString.toString().split("").join("-");
+  function stop() {
+    elements.forEach((element) => {
+      resizeObserver.unobserve(element);
+    });
+  }
+  return { start, stop };
 }
-
-elements.forEach((element) => {
-  resizeObserver.observe(element);
-});
